9136dcbc5981aeb28fe5332d2a144de9482e75ca
maxb00 <maxbar00@yahoo.com>
2015-09-25 14:39:02 -0400

    Combined classes for learning purposes, started math

--- Hello.py
+++ Hello.py
@@ -1,15 +1,17 @@
 print ("Init sucessful")
 run = True
-goAgain = 1
-while run == True & goAgain == 1:
-    print ("Testing with Python and Git...")
-    fdbck = input("How are we doing? ")
-    print ("Thank you for the '"+fdbck+"'")
-    print ("I welcome any feedback, I'll get there eventually!")
-    again = input("Anything more to say? Want to try again? (yes/no)")
-    if input == "yes":
-        goAgain = 1
+while run == True:
+    choice = input("What functionality do you want? Abs Value Finder, Feedback, or Exit?")
+
+    if choice == "Feedback" or choice == "feedback":
+        print ("Testing with Python and Git...")
+        fdbck = input("How are we doing? ")
+        print ("Thank you for the '"+fdbck+"'")
+        print ("I welcome any feedback, I'll get there eventually!")
+
+    elif choice == "Maths" or choice == "maths":
+        print("Absolute Value Finder + Other Math init sucessful")
+        print(abs(int(input("What number do you want the absolute value of?"))))
+
     else:
         run = False
-        goAgain = 0
-
