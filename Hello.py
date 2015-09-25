801806b74c507545ebc4115ae1aeb464ae7e151c
maxb00 <maxbar00@yahoo.com>
2015-09-25 14:22:41 -0400

    Created Basic feedback responder

--- Hello.py
+++ Hello.py
@@ -1,1 +1,15 @@
+print ("Init sucessful")
+run = True
+goAgain = 1
+while run == True & goAgain == 1:
+    print ("Testing with Python and Git...")
+    fdbck = input("How are we doing? ")
+    print ("Thank you for the '"+fdbck+"'")
+    print ("I welcome any feedback, I'll get there eventually!")
+    again = input("Anything more to say? Want to try again? (yes/no)")
+    if input == "yes":
+        goAgain = 1
+    else:
+        run = False
+        goAgain = 0
 
